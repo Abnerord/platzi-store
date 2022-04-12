2386e9f21d8266dfa8410549d42e18d46b9869de
Abner Rodriguez <abner.ord@gmail.com>
2022-04-12 12:23:13 -0600

    En este momento se introdujo codigo POST para saber como atender el metodo POST.

---
+++ src/controllers/products.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+    @Get()
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return { message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`, };
+      
+  }
+
+  @Get('filter')
+  getProductFilter() {
+    return `yo soy un filter`;
+  }
+
+  @Get(':productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+@Post()
+  create(@Body() payload: any){
+      return {
+          message: `accion de crear`,
+          payload
+      };
+  }
+  
+}
