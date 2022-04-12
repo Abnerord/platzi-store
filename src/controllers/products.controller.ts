b34944a256d74f361cb50eb95fecdff84aba994d
Abner Rodriguez <abner.ord@gmail.com>
2022-04-12 12:38:54 -0600

    Utilizamos el metodo put y delete aprendidmos a generar estos parametros.

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body, Put, Delete } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -30,4 +30,17 @@ export class ProductsController {
       };
   }
   
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any){
+    return{
+      id,
+      payload
+    }
+  };
+
+  @Delete(':id')
+  delete(@Param('id') id: number){
+    return id;
+  }
 }
